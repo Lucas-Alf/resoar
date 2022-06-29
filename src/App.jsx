9c2737905d1275fdc768512477a8c0f9ba671693
Lucas Alf <lucas.alf@voalle.com.br>
2022-06-29 13:48:23 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div class='center-text'>
+        <h1>🏗️ Under development</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
